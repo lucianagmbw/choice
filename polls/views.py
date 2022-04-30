c07373ef06ffa912be29e96f07bc5c69094cfd9c
unknown <lucianagmbw@gmail.com>
2022-04-29 21:41:25 -0300

    database sqlite and polls-tests

--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,36 @@
-from django.http import Http404
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.template import loader
-from .models import Choice, Question
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
 
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+def get_queryset(self):
+    #Return the last five published questions (not including those set to be
+    #published in the future).   
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not Exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    ... # same as above, no changes needed.
 
 
 def vote(request, question_id):
