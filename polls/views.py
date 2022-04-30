546582fdd1be07d354a827de87cb829251d14cb2
unknown <lucianagmbw@gmail.com>
2022-04-30 17:45:43 -0300

    test

--- polls/views.py
+++ polls/views.py
@@ -11,10 +11,9 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-def get_queryset(self):
-    #Return the last five published questions (not including those set to be
-    #published in the future).   
-    return Question.objects.filter(
+    def get_queryset(self):
+   
+        return Question.objects.filter(
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')[:5]
 
@@ -55,3 +54,6 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+#-------
+
